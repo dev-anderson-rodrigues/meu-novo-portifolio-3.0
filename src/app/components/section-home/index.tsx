afcada7c4545877432d67387df33f1a63d3298f7
anderson <andersoncassio2008@gmail.com>
2024-10-05 01:06:30 -0300

    "refactor": correção css home

--- src/app/components/section-home/index.tsx
+++ src/app/components/section-home/index.tsx
@@ -8,7 +8,7 @@ const SectionHome = () => {
 
   return (
     <section
-      className="max-w-screen items-center min-h-screen flex justify-center"
+      className="max-w-screen min-h-screen flex items-center relative "
       id="home"
     >
       <div
@@ -22,41 +22,43 @@ const SectionHome = () => {
           width={1920}
           height={1080}
           quality={100}
-          className="max-h-screen"
+          className="max-h-screen min-h-96 pt-16"
         />
       </div>
-      <div className="mask absolute z-10">
-        <Image
-          src={"/assets/images/mask-banner.png"}
-          alt="mask-banner"
-          width={1920}
-          height={666}
-          quality={100}
-          className="object-cover"
-        />
-      </div>
-      <div className="macbookBg  z-10 absolute">
-        <Image
-          src={"/assets/images/macbook.png"}
-          alt="macbook"
-          width={1009}
-          height={372}
-          quality={100}
-        />
-      </div>
-      <div className="containerPrimary">
-        <h3 className=" text-xl md:text-6xl">Desenvolvendo Ideias</h3>
-        <h4 className="text-base md:text-4xl">Solucionando Desafios</h4>
-      </div>
-      <div className="btn">
-        <a
-          href="https://api.whatsapp.com/send?phone=5548988563774&text=SUA_MENSAGEM"
-          target="true"
-        >
-          <button className="lg:flex justify-center text-center items-center w-full h-full buttonContact">
-            Me Contate
-          </button>
-        </a>
+
+      <div className="macbookBg z-40 flex flex-col m-auto items-center  gap-7">
+        <div className="flex flex-col text-center mt-16">
+          <h3 className=" text-xl md:text-6xl">Desenvolvendo Ideias</h3>
+          <h4 className="text-base md:text-4xl">Solucionando Desafios</h4>
+        </div>
+        <div className="p-4">
+          <a
+            href="https://api.whatsapp.com/send?phone=5548988563774&text=SUA_MENSAGEM"
+            target="true"
+          >
+            <button className="lg:flex justify-center text-center items-center w-full h-full buttonContact">
+              Me Contate
+            </button>
+          </a>
+        </div>
+        <div className="w-screen relative flex items-center justify-center container-3">
+          <Image
+            src={"/assets/images/macbook.png"}
+            alt="macbook"
+            width={1009}
+            height={372}
+            quality={100}
+            className="absoute object-cover bottom-0 z-50 p-10 pb-0 macbook"
+          />
+          <Image
+            src={"/assets/images/mask-banner.png"}
+            alt="mask-banner"
+            width={1920}
+            height={666}
+            quality={100}
+            className="object-cover w-screen absolute bottom-0 -z-10"
+          />
+        </div>
       </div>
     </section>
   );
